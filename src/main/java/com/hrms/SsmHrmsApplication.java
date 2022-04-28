f2aeb022566601a736d475b18787210aa4556516
ian <ianaa10716@gmail.com>
2022-04-28 21:29:50 +0800

    重構成為Spring boot  +thymeleaf的形式

---
+++ src/main/java/com/hrms/SsmHrmsApplication.java
@@ -0,0 +1,13 @@
+package com.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmHrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmHrmsApplication.class, args);
+	}
+
+}
